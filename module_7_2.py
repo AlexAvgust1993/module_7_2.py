5d26eb6a6d83b361cf1503326ccb09b4eb4ad38a
Alex <alexavgust1993@gmail.com>
2024-08-29 21:04:21 +0300

    Update module_7_2.py

--- module_7_2.py
+++ module_7_2.py
@@ -4,11 +4,13 @@ from pprint import pprint
 
 def custom_write(file_name, strings):
     strings_positions = {}
+
     with open(file_name, 'w', encoding='utf-8') as file:
-        for i, string in enumerate(strings):
+        for string in strings:
+            start_pos = file.tell()
             file.write(string + '\n')
-            strings_positions[i, file.tell()] = string
-    file.close()
+            strings_positions[len(strings_positions) + 1, (start_pos)] = string
+
     return strings_positions
 
 
